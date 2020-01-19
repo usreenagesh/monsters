780993ee2fffd0094fbce5316372f907e0bf3028
usreenagesh <usreenagesh@gmail.com>
2020-01-18 20:17:22 -0500

    Updates

---
+++ precache-manifest.815868a0fe7bbe79c6ed7a31a7b14b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e128b20d29f78382554c2a4a4b48c5f",
+    "url": "/usreenagesh/monsters/index.html"
+  },
+  {
+    "revision": "544ea293f6cad6e5b521",
+    "url": "/usreenagesh/monsters/static/css/main.d5f1d409.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/usreenagesh/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/usreenagesh/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "544ea293f6cad6e5b521",
+    "url": "/usreenagesh/monsters/static/js/main.e4828af3.chunk.js"
+  },
+  {
+    "revision": "735e96b39bdefe22f11d",
+    "url": "/usreenagesh/monsters/static/js/runtime-main.7821b532.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/usreenagesh/monsters/precache-manifest.815868a0fe7bbe79c6ed7a31a7b14b17.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/usreenagesh/monsters/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
